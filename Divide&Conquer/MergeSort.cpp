f478aad0c0386ddf4eabc0a4f4850fbdde2d5bd7
Surya Shrivastava <suryashrivatava019@gmail.com>
2025-12-08 08:58:36 +0530

    Added files

--- Divide&Conquer/MergeSort.cpp
+++ Divide&Conquer/MergeSort.cpp
@@ -1,41 +1,63 @@
+// DIVIDE & CONQUER (MERGE SORT) ....By Surya Shrivastava.
 #include <iostream>
+#include <vector>
 using namespace std;
-int Merge(vector<int> vec, int stidx, int endidx, int mid) {
+void Merge(vector<int>& vec, int stidx, int endidx, int mid) {
+    vector<int> temp;
     int i = stidx;
     int j = mid + 1;
-    vector<int> temp;
-    while(i < = mid && j < = endidx) {
-        if(vec[i] >= vec[j]) {
-            temp.push_back(vec[j]);
-            j ++ ;
-        }else {
+    while(i <= mid && j <= endidx) {
+        if(vec[i] <= vec[j]) {
             temp.push_back(vec[i]);
             i ++ ;
+        }else {
+            temp.push_back(vec[j]);
+            j ++ ;
         }
     }
-    while(i < = mid) {
+    while(i <= mid) {
         temp.push_back(vec[i]);
         i ++ ;
     }
-    while(j < = endidx) {
+    while(j <= endidx) {
         temp.push_back(vec[j]);
         j ++ ;
     }
-    for ()
+    for (int idx = stidx , x = 0 ; idx <= endidx; idx ++ , x ++) {
+        vec[idx] = temp[x];
+    }
 }
-void MergeSort(vector<int> vec, int stidx, int endidx) {
+void print_vec(vector<int>& vec, int n) {
+    for (int i = 0; i < n; i ++) {
+        cout << vec[i] << " ";
+    }
+}
+void MergeSort(vector<int>& vec, int stidx, int endidx) {
+    if (stidx >= endidx) {
+        return;
+    }
     int mid = (stidx + endidx) / 2;
     // mid = stidx + (endidx - stidx)/2;
 
-    // for left
+    // for left half
     MergeSort(vec, stidx, mid);
 
-    //for right
+    //for right half
     MergeSort(vec, mid + 1, endidx);
 
-    // Combine 
+    // Combine left & right
     Merge(vec, stidx, endidx, mid);
 }
 int main() {
-
+    int n;
+    cout << "ENTER SIZE OF A VECTOR : ";
+    cin >> n;
+    vector<int> vec(n);
+    cout << "ENTER ELEMENTS OF A VECTOR : ";
+    for (int i = 0; i < n; i ++) {
+        cin >> vec[i];
+    }
+    MergeSort(vec, 0, n - 1);
+    print_vec(vec, n);
+    return 0;
 }

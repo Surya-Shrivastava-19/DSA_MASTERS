ed7e7c84dd58ddbd8c21820f1618c958cae497cc
Surya Shrivastava <suryashrivatava019@gmail.com>
2025-12-09 16:23:55 +0530

    Added files

--- Divide&Conquer/SearchinRotatedSorted.cpp
+++ Divide&Conquer/SearchinRotatedSorted.cpp
@@ -1,6 +1,14 @@
+// DIVIDE & CONQUER (SEARCH IN ROTATED SORTED ARRAY) ....By Surya Shrivastava.
+// TIME COMPLEXITY : O(logn)
 #include <iostream>
+#include <vector>
 using namespace std;
 int Searchin_RSA(vector<int> vec, int stidx, int endidx, int target) {
+    // Base case 
+    if (stidx > endidx) {
+        cout << "ELEMENT NOT OCCURS IN THIS VECTOR" << "\n";
+        return -1;
+    }
     // int mid = (stidx + endidx) / 2;
     int mid = stidx + (endidx - stidx) / 2;
     if (vec[mid] == target) {
@@ -18,7 +26,11 @@ int Searchin_RSA(vector<int> vec, int stidx, int endidx, int target) {
     }else {
         // Mid lie in Line 2
         if (vec[mid] <= target && target <= vec[endidx]) {
-            
+            // Call right half
+            Searchin_RSA(vec, mid + 1, endidx, target);
+        }else {
+            // Call left half
+            Searchin_RSA(vec, stidx, mid - 1, target);
         }
     }
 }
@@ -34,6 +46,7 @@ int main() {
     int target;
     cout << "ENTER TARGETED ELEMENT : ";
     cin >> target;
-    Searchin_RSA(vec, 0, n-1, target);
+    int i = Searchin_RSA(vec, 0, n-1, target);
+    cout <<"ELEMENT FOUND AT THE INDEX : " << i;
     return 0;
 }

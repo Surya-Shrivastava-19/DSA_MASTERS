0e5d524071441c9cafd67bf067ceec1db490646d
Surya Shrivastava <suryashrivatava019@gmail.com>
2025-12-10 07:32:27 +0530

    Added files

---
+++ Backtracking/BacktrackingonArray.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <vector>
+using namespace std;
+void printArray(vector<int>& vec, int n) {
+    for (int i=0; i<n; i++) {
+        cout << vec[i] << " ";
+    }
+}
+
+void ChangeArray(vector<int>& vec, int n, int i) {
+    if (i == n) {
+        printArray(vec, n);
+        cout << "\n";
+        return;
+    }
+    vec[i] = i + 1;
+    ChangeArray(vec, n, i + 1);
+    vec[i] = vec[i] - 2;
+}
+
+int main() {
+    vector<int> vec(5);
+    int n = 5;
+
+    ChangeArray(vec, n, 0);
+    printArray(vec, n);
+    return 0;
+}

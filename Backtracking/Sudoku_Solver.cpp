59b42ed47dce6b1a3012800d01b997f892398064
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-01-01 21:13:10 +0530

    Added files

--- Backtracking/Sudoku_Solver.cpp
+++ Backtracking/Sudoku_Solver.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void Print_Sudoku(vector<vector<int>> sudoku[9][9]) {
+void Print_Sudoku(vector<vector<int>> sudoku[][9]) {
     for (int i = 0; i < 9; i++) {
         for (int j = 0; j < 9; j++) {
             cout << sudoku[i][j] << " ";
         }
         cout << "\n";
+    }
+}
+bool SudokuSolver(vector<vector<int>> sudoku, int row, int col) {
+    if (row == 9) {
+        // SUDOKU SOLVE
+        Print_Sudoku(sudoku);
+        return true;
+    }
+    int Nextrow = row;
+    int Nextcol = col + 1;
+    if (col + 1 == 9) {
+        Nextrow = row + 1;
+        Nextcol = 0;
+    }
+    if (sudoku[row][col] != 0) {
+        SudokuSolver(sudoku, Nextrow, Nextcol);
+    }
+    for(int digit = 0; digit < 9; digit++) {
+        if (isSafe(sudoku, row, col, digit)) {
+            sudoku[row][col] = digit;
+            SudokuSolver(sudoku, Nextrow, Nextcol);
+        }
     }
 }
 int main() {
@@ -18,7 +40,7 @@ int main() {
                                         {9, 6, 0, 4, 0, 5, 3, 0, 0}
                                         {0, 3, 0, 0, 7, 2, 0, 0, 4}
                                         {0, 4, 9, 0, 3, 0, 0, 5, 7}
-                                        {8, 2, 7, 0, 0, 9, 0, 1, 3}};
-    Print_Sudoku(sudoku);      
+                                        {8, 2, 7, 0, 0, 9, 0, 1, 3}};  
+    SudokuSolver(sudoku, 0, 0);   
     return 0;                          
 }

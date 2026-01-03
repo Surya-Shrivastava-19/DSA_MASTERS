a9c5c5beb9c12bf2604cfaf35e11d4c21ec9378f
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-01-04 00:14:12 +0530

    Added files

--- Linked_list/list_stl.cpp
+++ Linked_list/list_stl.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 #include <list>
+#include <iterator>
 using namespace std;
 int main() {
     list<int> ll;
-    ll.push_front(2);
-    ll.push_front(1);
-    ll.push_front(3);
-    ll.push_front(4);
+    ll.push_front(2); // 1
+    ll.push_front(1); // 1 -> 2
+
+    ll.push_back(3); // 1 -> 2 -> 3
+    ll.push_back(4); // 1 -> 2 -> 3 -> 4
+    list<int>::iterator itr;
+    for (itr = ll.begin(); itr != ll.end(); itr++) {
+        cout << (*itr) << " -> ";
+    }
+    cout << "NULL";
     return 0;
 }

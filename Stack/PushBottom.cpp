466d4c83f4e873707ffb980530de5611d82369e5
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-01-25 11:48:23 +0530

    Add File

---
+++ Stack/PushBottom.cpp
@@ -0,0 +1,36 @@
+// Push at Bottom of Stack Using Backtracking .
+#include <iostream>
+#include <stack>
+using namespace std;
+void Push_At_Bottom(stack<int> &Stack, int val) {
+    if (Stack.empty()) {
+        Stack.push(val);     // push at top = push at bottom
+        return;
+    }
+    int temp = Stack.top();
+    Stack.pop();
+
+    Push_At_Bottom(Stack, val);
+    Stack.push(temp);
+}
+
+int main() {
+    stack<int> Stack;
+    Stack.push(700);
+    Stack.push(600);
+    Stack.push(500);
+    Stack.push(400);
+    Stack.push(300);
+    Stack.push(200);
+    Stack.push(100);
+    int n;
+    cout << "Enter the value to be pushed onto the Bottom of Stack : ";
+    cin >> n;
+    Push_At_Bottom(Stack, n);
+    cout << "Stack after pushing in the bottom : " << "\n";
+    while(!Stack.empty()) {
+        cout << Stack.top() << "\n";
+        Stack.pop();
+    }
+    return 0;
+}

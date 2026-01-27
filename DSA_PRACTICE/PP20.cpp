d7fb979f0759a6718069bbeafcee1cae972e299c
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-01-27 22:16:55 +0530

    Add File

---
+++ DSA_PRACTICE/PP20.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <list>
+#include <iterator>
+using namespace std;
+int main() {
+    list<int> ll;
+    ll.push_front(2); // 1
+    ll.push_front(1); // 1 -> 2
+
+    ll.push_back(3); // 1 -> 2 -> 3
+    ll.push_back(4); // 1 -> 2 -> 3 -> 4
+    ll.push_back(6);
+    list<int>::iterator itr;
+    for (itr = ll.begin(); itr != ll.end(); itr++) {
+        cout << (*itr) << " -> ";
+    }
+    cout << "NULL" << "\n";
+    cout << "SIZE OF A LINKED LIST : " << ll.size() << "\n";
+    cout << "FRONT VALUE OF A LINKED LIST : " << ll.front() << "\n";
+    cout << "BACK VALUE OF A LINKED LIST : " << ll.back() << "\n";
+    //
+    ll.insert(ll.begin(), 100); 
+    //
+    ll.pop_front();
+    ll.pop_back();
+    return 0;
+}

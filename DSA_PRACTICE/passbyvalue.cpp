707e231df6b66433ea905963f110e73a781bbff6
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-03-04 10:37:36 +0530

    Add File

---
+++ DSA_PRACTICE/passbyvalue.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+fun(int x) {
+    x = x + 10;
+    cout << "x = " << x << "\n";
+}
+int main() {
+    int a = 10;
+    fun(a);
+    cout << "a = " << a;
+    return 0;
+}

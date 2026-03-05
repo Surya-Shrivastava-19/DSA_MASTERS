9cfe83f394359ab2371347329522612440f31a32
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-03-05 20:37:26 +0530

    Add File

--- OOPS/M_Ambiguity.cpp
+++ OOPS/M_Ambiguity.cpp
@@ -1,3 +1,26 @@
 #include <iostream>
 using namespace std;
-class 
+class A {
+    public:
+    void show() {
+        cout << "CLASS A SHOW..." << "\n";
+    }
+};
+class B {
+    public:
+    void show() {
+        cout << "CLASS B SHOW..." << "\n";
+    }
+};
+class C: public A, public B {
+    public:
+    void func() {
+        cout << "THIS IS A FUNCTION." << "\n";
+    }
+};
+int main() {
+    C obj;
+    obj.A::show();
+    obj.B::show();
+    return 0;
+}

ee09786e2e534abd86050c2e824d4abd0cc4a7d9
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-04-25 21:40:27 +0530

    Add file

--- QUEUE/QueueusingLL.cpp
+++ QUEUE/QueueusingLL.cpp
@@ -26,7 +26,38 @@ class Queue{
             tail = tail -> next;
         }
     }
+    void pop() {
+        if(head == NULL){
+            cout << "QUEUE IS EMPTY";
+            return;
+        }else {
+            Node* temp = head;
+            head = head -> next;
+            temp -> next = NULL;
+            delete temp;
+        }
+    }
+    int front() {
+        if(head == NULL) {
+            cout << "QUEUE IS EMPTY";
+            return -1;
+        }
+        return (head -> data);
+    }
+    bool empty() {
+        return (head == NULL);
+    }
 };
 int main() {
-
+    Queue Q1;
+    Q1.push(1);
+    Q1.push(2);
+    Q1.push(3);
+    Q1.push(4);
+    Q1.push(5);
+    while(!empty()) {
+        cout << Q1.front() << "\n";
+        Q1.pop();
+    }
+    return 0;
 }

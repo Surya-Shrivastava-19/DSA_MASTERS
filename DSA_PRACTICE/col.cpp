6ca9073cfdffa14ea14e091049716ecf203fed43
Surya Shrivastava <suryashrivatava019@gmail.com>
2026-04-29 09:42:23 +0530

    Add file

--- DSA_PRACTICE/col.cpp
+++ DSA_PRACTICE/col.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 void Sum(int arr[], int key) {
+    int count = 0;
     for(int i = 0; i < 6; i++) {
-        for(int j = i + 1; j < 6; j++) {  // duplicate avoid करने के लिए
+        for(int j = i + 1; j < 6; j++) {
             if(arr[i] + arr[j] == key) {
+                count++;
                 cout << arr[i] << ", " << arr[j] << "\n";
             }
         }
     }
+    cout << "count: " << count << "\n";
 }
 
 int main() {
